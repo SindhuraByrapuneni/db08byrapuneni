8e5c66ff66d4dbf94172adef5fbb61f59c41eb90
Byrapuneni Sindhura <89601758+SindhuraByrapuneni@users.noreply.github.com>
2021-11-12 09:11:33 -0600

    Add files via upload

--- routes/resource.js
+++ routes/resource.js
@@ -27,6 +27,43 @@ dog_controller.dog_update_put);
 router.get('/dog/:id', dog_controller.dog_detail); 
  
 // GET request for list of all dog items. 
-router.get('/dog', dog_controller.dog_list); 
+router.get('/dog', dog_controller.dog_list);
+// GET request for one dog. 
+router.get('/dogs/:id', dog_controller.dog_detail);
+// for a specific dog. 
+exports.dog_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await dog.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+};  
+
+
+//Handle dog update form on PUT. 
+exports.dog_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await dog.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.dog_type)  
+               toUpdate.dog_type = req.body.dog_type; 
+        if(req.body.cost) toUpdate.cost = req.body.cost; 
+        if(req.body.color) toUpdate.size = req.body.color; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`); 
+    } 
+    if(req.body.checkboxsale) toUpdate.sale = true; 
+else toUpdate.same = false; 
+ 
+}; 
  
 module.exports = router;
